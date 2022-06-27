69263f5d2dfdb522acc97e1f2c5dd4d9c0e421d6
Tuong Tran Vi <tvtuong@tma.com.vn>
2022-06-27 11:32:15 +0700

    config webpack,scss,babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import "./App.scss";
 const el = document.getElementById("app");
 const root = createRoot(el); 
