7ee36a0bddc8c97b02ecfd8b86b0b1a16ba83921
Tuong Tran Vi <tvtuong@tma.com.vn>
2022-06-27 13:47:20 +0700

    config node sass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from "./App";
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import "./App.scss";
-const el = document.getElementById("app");
-const root = createRoot(el); 
+import './App.scss';
+const el = document.getElementById('app');
+const root = createRoot(el);
 root.render(<App />);
--- src/testTypeScript/main.ts
+++ src/testTypeScript/main.ts
@@ -1,20 +1,8 @@
-// Here we define our Food interface, its properties, and their types.
-interface Food {
-    name: string;
-    calories: number;
+var burger: string = 'hambuger',
+  calories: number = 300,
+  tasty: boolean = true;
+function speak(food: string, energy: number): void {
+  console.log(`Our ${food} has ${energy} calories`);
 }
 
-// We tell our function to expect an object that fulfills the Food interface. 
-// This way we know that the properties we need will always be available.
-function speak(food: Food): void{
-  console.log("Our " + food.name + " has " + food.calories + " calories.");
-}
-
-// We define an object that has all of the properties the Food interface expects.
-// Notice that types will be inferred automatically.
-var ice_cream = {
-  name: "ice cream", 
-  calories: 200
-}
-
-speak(ice_cream);
+speak(burger, calories);
