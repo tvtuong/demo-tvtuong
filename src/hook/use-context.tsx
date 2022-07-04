062dcf6be87c21ac903bb4f9c15abd7ed9c6523e
Tuong Tran Vi <tvtuong@tma.com.vn>
2022-07-04 09:59:02 +0700

    sync code

--- src/hook/use-context.tsx
+++ src/hook/use-context.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-interface User {
-  name: string;
-  age: number;
-}
+// interface User {
+//   name: string;
+//   age: number;
+// }
 
-const UserContext = React.createContext<User[] || []>([]);
+// const UserContext = React.createContext<User[{name: }] || []>([]);
 
-const ExampleUseContext : React.FC = () => {
-  const userList = React.useState<User[] | []>([{name: 'User 01', age: 15}])
-}
+// const ExampleUseContext : React.FC = () => {
+//   const userList = React.useState<User[] | []>([{name: 'User 01', age: 15}])
+// }
 
-export default ExampleUseContext;
+// export default ExampleUseContext;
