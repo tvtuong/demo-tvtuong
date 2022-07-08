32f04063f015fddc40af0d523eafbd8ffb6432da
Tuong Tran Vi <tvtuong@tma.com.vn>
2022-07-08 09:30:49 +0700

    config redux-form

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counters/counterSlice';
+import { reducer as formReducer } from 'redux-form';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    form: formReducer,
   },
 });
 //Suy ra Types `RootState` và` AppDispatch` từ store
